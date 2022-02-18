e883973218e3639202bfd4498abfbe1160f413a3
EduardoPena27 <78942558+EduardoPena27@users.noreply.github.com>
2022-02-18 12:59:31 -0600

    hol

--- PiedraPapel/app/src/main/java/com/example/piedrapapel/MainActivity.java
+++ PiedraPapel/app/src/main/java/com/example/piedrapapel/MainActivity.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    //hola
     int cont=0, contPiedra=0, contPapel=0, contTijeras=0, contLagarto=0, contSpock=0;
     ImageButton btiPiedra, btiPapel, btiTijeras, btiLagarto, btiSpock;
     TextView tvGanador;
